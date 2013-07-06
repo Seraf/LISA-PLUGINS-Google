8ec6d81901dd640dcbc680a794bebc51475a2586
Seraf <julien.syx@gmail.com>
2013-07-06 18:00:38 +0200

    Changing the way the module is loaded

--- lang/fr/google.rs
+++ lang/fr/google.rs
@@ -2,12 +2,6 @@
 - <call>getcalendars <star></call>
 
 > object getcalendars python
-    import sys, os, inspect
-    cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split( \
-        inspect.getfile(inspect.currentframe()))[0],os.path.normpath("Plugins/Google/modules/"))))
-    if cmd_subfolder not in sys.path:
-        sys.path.insert(0, cmd_subfolder)
-
-    from google import Google
+    from Google.modules.google import Google
     return Google().getCalendars(args)
 < object
